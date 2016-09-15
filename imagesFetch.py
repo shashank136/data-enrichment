e66e28f89b93ccbc3f7b37e83077b4eb36f13157
Shubhankar Dimri <dimrishubhi@gmail.com>
2016-09-15 17:41:08 +0530

    Fixes the issue of IOError
    That issue arose because the server didn't had the font file

--- imagesFetch.py
+++ imagesFetch.py
@@ -2,24 +2,27 @@ from PIL import Image,ImageDraw,ImageFont
 import random
 import re
 import os
+import glob
 def fetch(s):
 	li=""
+	add=glob.glob("./output/images")
 	x=re.compile(r'[A-Z]')
 	x=x.findall(s)
 	for i in range(len(x)):
 		li+=x[i]
 	W,H=(250,250)
 	base=Image.new("RGBA",(250,250),(random.randrange(170,255),random.randrange(70,255),random.randrange(70,255)))
-	fnt=ImageFont.truetype('Pillow/Tests/fonts/FreeMonoBold.ttf',50)
+	font_path=os.environ.get("font_path","./fonts/DroidSans-Bold.ttf")
+	fnt=ImageFont.truetype(font_path,50)
 	w,h=fnt.getsize(li)
 	d=ImageDraw.Draw(base)
 	d.text(((W-w)/2,(H-h)/2),li,font=fnt,fill="white")
 	try:
-		os.makedirs("./output/images")
+		os.makedirs(add)
 	except Exception:
 		pass
+	#add=glob.glob("./output/images/")
 	base.save("./output/images/"+s+".png")
 	return "./output/images/"+s+".png"
-
 
 
