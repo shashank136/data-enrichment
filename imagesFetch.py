43f1559828137d9833644635f9564065d09b7e76
Shubhankar Dimri <dimrishubhi@gmail.com>
2016-09-16 17:11:17 +0530

    Changed the size of image to 500x500 #19
    Added ability to auto resize image in case text width increases image width #19

--- imagesFetch.py
+++ imagesFetch.py
@@ -10,19 +10,21 @@ def fetch(s):
 	x=x.findall(s)
 	for i in range(len(x)):
 		li+=x[i]
-	W,H=(250,250)
-	base=Image.new("RGBA",(250,250),(random.randrange(170,255),random.randrange(70,255),random.randrange(70,255)))
+	W,H=(500,500)
+	base=Image.new("RGBA",(W,H),(random.randrange(170,255),random.randrange(70,255),random.randrange(70,255)))
 	font_path=os.environ.get("font_path","./fonts/DroidSans-Bold.ttf")
-	fnt=ImageFont.truetype(font_path,50)
+	fnt=ImageFont.truetype(font_path,100)
 	w,h=fnt.getsize(li)
+	if w>W:
+	    base=base.resize((W+(w-W)+20,H),Image.ANTIALIAS) #resizing image in case text width > image width
+	    W=W+(w-W)+20
 	d=ImageDraw.Draw(base)
 	d.text(((W-w)/2,(H-h)/2),li,font=fnt,fill="white")
 	try:
 		os.makedirs(add)
 	except Exception:
 		pass
-	#add=glob.glob("./output/images/")
-	base.save("./output/images/"+s+".png")
-	return "./output/images/"+s+".png"
+	#base.save("./output/images/"+s+".png")
+	return "static.careerbreeder.com/output/images/"+s+".png"
 
 
