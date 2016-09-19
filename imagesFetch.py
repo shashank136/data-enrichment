848259bfce428724890a5a63e0e44af34df6e2d0
Shubhankar Dimri <dimrishubhi@gmail.com>
2016-09-20 01:27:10 +0530

    refracted code and updated to save images

--- imagesFetch.py
+++ imagesFetch.py
@@ -10,7 +10,6 @@ def fetch(s):
 	for i in range(len(x)):
 		li+=x[i]
 	W,H=(850,450)
-	print 
 	base=Image.new("RGBA",(W,H),(random.randrange(170,255),random.randrange(70,255),random.randrange(70,255)))
 	font_path=os.environ.get("font_path","./fonts/DroidSans-Bold.ttf")
 	fnt=ImageFont.truetype(font_path,100)
@@ -20,23 +19,9 @@ def fetch(s):
 		W=W+(w-W)+20
 	d=ImageDraw.Draw(base)
 	d.text(((W-w)/2,(H-h)/2),li,font=fnt,fill="white")
+	try:
+		os.mkdir("./output/images")
+	except Exception:
+		pass
+	base.save("./output/images/"+s+".png")
 	return "static.careerbreeder.com/output/images/"+s+".png"
-
-def correctImage(photos):
-	'''
-	correcting thumbnails
-	'''
-	new_imgs=''
-	photos_li=list(map(str,list(photos.strip().split("#"))))
-	#print "photos_li",photos_li
-	match_str=re.compile(r'-t',re.I)
-	for i in range(len(photos_li)):
-		if match_str.search(photos_li[i].strip()):
-			photos_li[i]=''
-	for i in range(len(photos_li)):
-		if photos_li[i]!='' and i!=(len(photos_li)-1):
-			new_imgs+=photos_li[i]+","
-		elif photos_li[i]!='' and i==(len(photos_li)-1):
-			new_imgs+=photos_li[i]
-	return new_imgs
-
