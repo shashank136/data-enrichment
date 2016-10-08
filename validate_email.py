e0b984e92b74fa2e1038f85ab1719762714e14e0
gupta-shantanu <shantanu@programmer.net>
2016-10-08 23:52:46 +0530

    avoid exceptions from connection errors

--- validate_email.py
+++ validate_email.py
@@ -4,17 +4,20 @@ import smtplib
 def validate_email(email):
         if not 'gmail' in email:
                 return True #For now we are not filtering out non gmail addresses
-        records = dns.resolver.query(email[email.find('@')+1:], 'MX')
-        mxRecord = records[0].exchange
-        mxRecord = str(mxRecord)
-        host = socket.gethostname()
-        server = smtplib.SMTP()
-        server.set_debuglevel(0)
-        server.connect(mxRecord)
-        server.helo(host)
-        server.mail('me@domain.com')
-        code, message = server.rcpt(str(email))
-        server.quit()
-        if code == 250:
+        try:
+                records = dns.resolver.query(email[email.find('@')+1:], 'MX')
+                mxRecord = records[0].exchange
+                mxRecord = str(mxRecord)
+                host = socket.gethostname()
+                server = smtplib.SMTP()
+                server.set_debuglevel(0)
+                server.connect(mxRecord)
+                server.helo(host)
+                server.mail('me@domain.com')
+                code, message = server.rcpt(str(email))
+                server.quit()
+                if code == 250:
+                        return True
+                return False
+        except:
                 return True
-        return False
