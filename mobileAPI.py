6118960b493af5f9292009ace5e215b8285e0139
Shubhankar Dimri <dimrishubhi@gmail.com>
2016-10-11 21:30:12 +0530

    Handles some edge cases in phone nos
    handles '/', any name in phone nos and rectifies them
    Also removes duplicated numbers from mobiles column

--- mobileAPI.py
+++ mobileAPI.py
@@ -29,21 +29,80 @@ class processMobile:
         return { 'national_format': "xx xx xx", 'phone_number': phone_no}
 
     def _isMobile(self,phone_no):
+        phone_no=filter(lambda x: not x.isalpha(),str(phone_no))
         result = self._request(phone_no)
         try:
+            #print phone_no
             return result['national_format'].count(' ') == 1, result['phone_number']
         except:
             print "Bad Phone number "+str(phone_no)
             return False, phone_no
     
     def _addCountryCode(self,phone_no):
+        phone_no=str(phone_no)
+        phone_no=phone_no.replace(' ','').replace('(','').replace(')','')
+        if '0091' in phone_no[0:len(phone_no)/2+1]:
+            phone_no=phone_no.replace('0091','')
+        elif '091' in phone_no[0:len(phone_no)/2+1]:
+            phone_no=phone_no.replace('091','')
         if phone_no.startswith('0'):
             phone_no = phone_no[1:]
         if not phone_no.startswith('91') and not phone_no.startswith('+91'):
             return '+91-'+phone_no
         return phone_no
 
+    def checkNo(self,rows):
+        for row in rows:
+            noslist=[]
+            phn_no_ls=''
+            for i in range(1,6):
+                nphn=''
+                if row['Phone'+str(i)] is not None:
+                    row['Phone'+str(i)]=filter(lambda x: not x.isalpha(),str(row['Phone'+str(i)])).strip().replace(' ','')
+                    # nphn=row['Phone'+str(i)].replace(' ','').replace('(','').replace(')','').replace('-','')
+                    # if len(nphn)>=20:
+                    #     phn_no_ls+= 
+                    phn_no_ls+=row['Phone'+str(i)]+','
+
+            phn_no_ls=phn_no_ls.split(',')
+            phn_no_ls=filter(None,phn_no_ls)
+
+            #phn_no_ls=map(str,phn_no_ls)
+            
+            for i in range(len(phn_no_ls)):
+                if len(phn_no_ls[i])>=20:
+                    n=1
+                    if len(phn_no_ls[i])%2==0:
+                        n=0
+                    #print 'phn no',phn_no_ls[i]
+                    splt_no=phn_no_ls[i][0:-len(phn_no_ls[i])/2+n]+'/'+phn_no_ls[i][-len(phn_no_ls[i])/2+n:]
+                    phn_no_ls[i]=splt_no
+                    #print 'splt_no',splt_no
+            for no in phn_no_ls:
+                if '/' not in no:
+                    noslist.append(no)
+                elif '/' in no:
+                    splt_list=no.split('/')
+                    for i in range(1,len(splt_list)):
+                        if len(splt_list[i])>=10:
+                            continue
+                        splt_list[i]=splt_list[0][0:-len(splt_list[i])]+splt_list[i]
+                    noslist=noslist+splt_list
+            noslist=set(noslist)
+            noslist=list(noslist)
+            # for i in range(len(noslist)):
+            #     if len(noslist[i])>=20:
+            #         splt_no=noslist
+            #print 'No of noslist',len(noslist)
+            print noslist
+            for i in range(0,len(noslist)):
+                if i==5:
+                    break;
+                else:
+                    row['Phone'+str(i+1)]=noslist[i]
+
     def processAll(self,rows):
+        self.checkNo(rows)
         total=len(rows)
         for progress,row in enumerate(rows):
             mobiles=[]
@@ -57,7 +116,6 @@ class processMobile:
             pro=int((float(progress)/total)*100)
             sys.stdout.write("\r%d%%"%pro)
             sys.stdout.flush()
-            row['Mobiles'] = ','.join(mobiles)
+            row['Mobiles'] = ','.join(list(set(mobiles)))
             #print(row['Mobiles'])
             
-
