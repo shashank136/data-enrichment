9822ece9a993ad1d6c29500e338955e2f54657d6
Shashwat Vikram <shashwat.vikram50@gmail.com>
2016-10-26 03:54:53 +0530

    Fix Issue #63

--- mobileAPI.py
+++ mobileAPI.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from fbGraph import UTF8
+from fbGraph import safe_dec_enc
 import sys
 
 keys = [ ("AC086728331b2e303cda499de8e36e3ba2","2fd8e3dcad09495f4b68ba20eef63344"),
@@ -8,7 +8,7 @@ keys = [ ("AC086728331b2e303cda499de8e36e3ba2","2fd8e3dcad09495f4b68ba20eef63344
 key_index=0
 
 class processMobile:
-    def __init__(self,key=None):        
+    def __init__(self,key=None):
         self.key = keys[key_index]
         self.url = "https://lookups.twilio.com/v1/PhoneNumbers/%s"
 
@@ -37,9 +37,8 @@ class processMobile:
         except:
             print "Bad Phone number "+str(phone_no)
             return False, phone_no
-    
+
     def _addCountryCode(self,phone_no):
-        phone_no=unicode(phone_no)
         phone_no=phone_no.replace(' ','').replace('(','').replace(')','')
         if '0091' in phone_no[0:len(phone_no)/2+1]:
             phone_no=phone_no.replace('0091','')
@@ -59,12 +58,12 @@ class processMobile:
             for i in range(1,6):
                 nphn=''
                 if row['Phone'+str(i)] is not None:
-                    y=(filter(lambda x: not x.isalpha(),unicode(row['Phone'+str(i)])).strip().replace(' ',''))
+                    y=(filter(lambda x: not x.isalpha(),safe_dec_enc(row['Phone'+str(i)])).strip().replace(' ',''))
                     noslist1.append(y)
                     row['Phone'+str(i)]=None
                     # nphn=row['Phone'+str(i)].replace(' ','').replace('(','').replace(')','').replace('-','')
                     # if len(nphn)>=20:
-                    #     phn_no_ls+= 
+                    #     phn_no_ls+=
                     # phn_no_ls+=row['Phone'+str(i)]+','
 
             noslist1=filter(None,noslist1);
@@ -77,7 +76,7 @@ class processMobile:
             phn_no_ls=filter(None,phn_no_ls)
 
             #phn_no_ls=map(str,phn_no_ls)
-            
+
             for i in range(len(phn_no_ls)):
                 if len(phn_no_ls[i])>=20:
                     n=1
@@ -99,7 +98,7 @@ class processMobile:
                     noslist=noslist+splt_list
             noslist=set(noslist)
             noslist=list(noslist)
-          
+
             for i in range(0,len(noslist)):
                 if i==5:
                     break;
@@ -112,10 +111,10 @@ class processMobile:
         for progress,row in enumerate(rows):
             mobiles=[]
             for i in range(1,6):
-                n = row["Phone"+str(i)]
+                n = safe_dec_enc(row["Phone"+str(i)])
                 if not n:
                     continue
-                isMob,cleaned_no = self._isMobile(self._addCountryCode(UTF8(n)))
+                isMob,cleaned_no = self._isMobile(self._addCountryCode(n))
                 if isMob:
                     cleaned_no= cleaned_no.replace('+91','')
                     if cleaned_no[0:2]=='91':
@@ -126,4 +125,3 @@ class processMobile:
 ##            sys.stdout.flush()
             row['Mobiles'] = ', '.join(list(set(mobiles)))
             #print(row['Mobiles'])
-            
