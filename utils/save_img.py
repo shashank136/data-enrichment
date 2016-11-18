2124888cb1fd6b6514953e73535d519793d3fafb
Shubhankar Dimri <dimrishubhi@gmail.com>
2016-11-18 08:18:59 -0800

    addresses #72

---
+++ utils/save_img.py
@@ -0,0 +1,94 @@
+import glob
+import hashlib
+import requests
+import csv
+import sys,os
+csv.field_size_limit(sys.maxsize)
+
+def UTF8(data):
+	try:
+		return data.encode('utf-8','ignore')
+	except:
+		return data
+
+class SaveImg():
+	def __init__(self):
+	    self.rows = []
+	    self.FIELDS = []
+	
+	def process(self):
+		fileNames=glob.glob('../output/updated_*.csv')
+		print fileNames
+		for fileName in fileNames:
+			print 'Cur file is',fileName
+			self.rows=[]
+			self.fields=[]
+			fileBaseName = os.path.splitext(os.path.basename(fileName))[0]
+			self._readCSV(fileName)
+			print len(self.rows)
+			self.save_img()
+			try:
+				os.mkdir('../output/new_output/')
+			except:
+				pass
+			self._writeCSV("../output/new_output/"+fileBaseName+".csv");
+	
+	def _readCSV(self, fileName):
+		inputFile = open(fileName, 'r')
+		reader = csv.DictReader(inputFile, dialect=csv.excel)
+		self.FIELDS = reader.fieldnames;
+		self.rows.extend(reader);
+		inputFile.close();
+	
+	def save_img(self):
+
+		for row in self.rows:
+			s=''
+			lis_imgs=[]
+			new_lis_imgs=[]
+			cont=''
+			h=hashlib.new('ripemd160')
+			s=row['listing_gallery']
+			lis_imgs=s.split(',')
+			print len(lis_imgs)
+			for i in lis_imgs:
+				if 'static.career' not in i and i!=' ':
+	  				h.update(row['post_title']+str(row['EduID'])+str(i))
+					cont=h.hexdigest()
+    				try:
+						req=requests.get(i)
+						if req.status_code==200:
+							print 'Downloading'
+							with open('../output/images/%s.jpg'%cont,'wb') as f:
+								f.write(req.content)
+	    						f.close()
+	    					new_lis_imgs.append('http://static.careerbreeder.com/output/images/%s.jpg'%cont)
+	    			
+	    			except Exception as err:
+	    				print err
+
+    		if len(new_lis_imgs)!=0:
+    			row['listing_gallery']=",".join(new_lis_imgs)
+    			row['featured_image']=row['listing_gallery'][0]
+	
+	def _writeCSV(self, fileName):
+		print "Writing to CSV..."
+		try:
+			csvFile = open(fileName, 'w');
+			writer = csv.DictWriter(csvFile, fieldnames=self.FIELDS);
+			writer.writerow(dict(zip(self.FIELDS, self.FIELDS)))
+			for row in self.rows:
+				for i in row:
+					row[i] = UTF8(row[i])
+				writer.writerow(row)
+			csvFile.close()
+		except Exception as err:
+			logging.exception("Something awful happened when processing result.csv");
+
+if __name__=='__main__':
+	f=SaveImg()
+	f.process()
+
+
+
+
