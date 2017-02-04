d77b9c706fb6d84d11d58facc12519408f37ce4c
Shashwat Vikram <shashwat.vikram50@gmail.com>
2017-02-04 23:44:11 +0530

    Changed normalize_city_name.py to run in main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import os
 
 os.system('python geocodertest.py')
 os.system('python update.py')
-
+os.system('python normalize_city_name.py')
--- normalize_city_name.py
+++ normalize_city_name.py
@@ -1,6 +1,6 @@
 import csv
 import glob,sys
-csv.field_size_limit(sys.maxsize)
+csv.field_size_limit(sys.maxint)
 
 class Normalize_City:
     def __init__(self):
@@ -9,14 +9,14 @@ class Normalize_City:
 
     def initUniverse(self):
         u_rows = []
-        with open('../city_alias/alias_data.csv', 'rb') as csvFile:
+        with open('city_alias/alias_data.csv', 'rb') as csvFile:
             reader = csv.DictReader(csvFile, dialect=csv.excel)
             u_rows.extend(reader)
         for row in u_rows:
             map_value = row['Current_Use']
             for key, value in row.items():
                 if key != 'Current_Use' and value:
-                    self.alias_universe[value] = map_value
+                    self.alias_universe[value.lower()] = map_value
 
     def read_csv(self,file_path,fields,rows):
         with open(file_path, 'rb') as csvFile:
@@ -33,8 +33,8 @@ class Normalize_City:
 
     def normalize(self, rows):
         for row in rows:
-            if row['City'] and self.alias_universe.get(row['City']):
-                row['City'] = self.alias_universe[row['City']]
+            if row['City'] and self.alias_universe.get(row['City'].lower()):
+                row['City'] = self.alias_universe[row['City'].lower()]
 
     def main(self, files):
         for file_path in files:
@@ -45,6 +45,7 @@ class Normalize_City:
             self.write_csv(file_path, fields, rows)
 
 if __name__ == '__main__':
-    files = glob.glob("../output/updated_*.csv")
+    print '\nNORMALIZING CITY NAMES'
+    files = glob.glob("output/updated_*.csv")
     obj = Normalize_City()
     obj.main(files)
