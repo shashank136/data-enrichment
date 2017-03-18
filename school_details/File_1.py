a0af9ff95c269729fe2f2ddf512b787e7d4f7946
Shashank Kumar <shashank136.sk@gmail.com>
2017-03-18 19:34:55 +0530

    Update File_1.py
    Added update for STD code.

--- school_details/File_1.py
+++ school_details/File_1.py
@@ -4,7 +4,7 @@ import File_2
 filename = "shashank_kumar.csv"
 f = open(filename, "w")
 
-headers = "School_Name, Affiliation_no, State, District, Postal_address, Pin_code, Phone_no, Email_id, Web_site, Year_of_foundation, Date_of_opening, Name_of_Principal, Sex, Status_of_affiliation, Type_of_affiliation, Affiliation_from, Affiliation_to, Name_of_trust_society_managing_committee\n"
+headers = "School_Name, Affiliation_no, State, District, Postal_address, Pin_code, STD_Code, Phone_no, Email_id, Web_site, Year_of_foundation, Date_of_opening, Name_of_Principal, Sex, Status_of_affiliation, Type_of_affiliation, Affiliation_from, Affiliation_to, Name_of_trust_society_managing_committee\n"
 
 f.write(headers)
 f.close()
