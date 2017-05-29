3f2ac2794249255f67081c33b20c0f540956d079
Shashank Kumar <shashank136.sk@gmail.com>
2017-05-29 20:40:21 +0530

    Added comments and made some suggested changes

--- mobileAPI.py
+++ mobileAPI.py
@@ -12,31 +12,34 @@ key_index=0
 
 class processMobileSTDcode:
 
-    def processSTD(self,city,n):
+    def processSTD(self,city,telephone):
 
-        telephone = n
-        ph_len = len(telephone.replace('-',''))
+        ph_len = len(telephone.replace('-','').repace(" ","")) # remove extra space and invaid characters e.g. '54-66121  3231'
         # print("length of telephone number is: "+str(ph_len))
 
         readfile = open('std_data.csv',"r")
         filereader = csv.reader(readfile)
 
         if ph_len < 9:
-            city = ro['City']
+            # city = ro['City']
 
-            for row in filereader:
-                for field in row:
+            for row in filereader: # here row refers to a row in the csv file of std_data.csv
+                for field in row:  # iterating over various fields in csv file which are city, circle and std
                     if field == city.upper():
-                        stdno = eval(row[2])
+                        stdno = eval(row[2]) # fetching std code
                         # print(stdno)
                         # print(len(str(stdno)))
-                        x=len(str(stdno))
-                        if x==2:
-                            std_str = '%0*d' % (3,stdno)
-                        elif x==3:
-                            std_str = '%0*d' % (4,stdno)
+                        std_len = len(str(stdno))
+                        
+                        if std_len == 2:
+                            std_str = '%0*d' % (3,stdno) # e.g. converts 11 to 011
+                        
+                        elif std_len == 3:
+                            std_str = '%0*d' % (4,stdno) # e.g. converts 524 to 0524
+                        
                         else:
-                            std_str = '%0*d' % (5,stdno)
+                            std_str = '%0*d' % (5,stdno) # e.g. converts 1254 to 01254
+                        
                         print(std_str)
                         # print(len(std_str))
                     
@@ -44,56 +47,55 @@ class processMobileSTDcode:
                         # print(new_tele)
 
                         if len(new_tele) == 11:
-                            telephone = new_tele
-                            return telephone
-                            # print('telephone no: '+telephone)
+                             return new_tele 
+                            # print('telephone no: '+ new_tele)
                         else:
-                            telephone = 'NA'
-                            return telephone
-                            # print('telephone no: '+telephone)
+                            new_tele = ''
+                            return new_tele
+                            # print('telephone no: '+ new_tele)
 
         readfile.close()
 
 class processMobile:
     def __init__(self,key=None):
-        self.key = keys[key_index]
-        self.url = "https://lookups.twilio.com/v1/PhoneNumbers/%s"
+        self.key = keys[key_index]  # picks up a key from keys list
+        self.url = "https://lookups.twilio.com/v1/PhoneNumbers/%s" # URL being used for verification of PhoneNumber
 
-    def _request(self,phone_no):
-        global key_index
-        start = key_index
-        for i in range(len(keys)):
+    def _request(self,phone_no): # This function verify if phone number working using Keys 
+        global key_index # key_index =0
+        start = key_index # start = 0
+        for i in range(len(keys)): # iterating through keys 
             try:
-                return json.loads(requests.get(self.url%phone_no,auth=self.key).text)
+                return json.loads(requests.get(self.url%phone_no,auth=self.key).text) # responce in json format 
             except:
-                key_index = (key_index + 1) % len(keys)
+                key_index = (key_index + 1) % len(keys) # changing key
                 print("Switching to new key"+str(key_index))
                 if start!=key_index:
-                    key = keys[key_index]
+                    key = keys[key_index] # taking up new key
                 else:
                     break
         print "None of the keys worked!"
         return { 'national_format': "xx xx xx", 'phone_number': phone_no}
 
-    def _isMobile(self,phone_no):
-        phone_no=filter(lambda x: not x.isalpha(),str(phone_no))
-        result = self._request(phone_no)
+    def _isMobile(self,phone_no): # verify if phone_no is in correct format e.g. 703xxxxxxx else report as bad phone no.
+        phone_no=filter(lambda x: not x.isalpha(),str(phone_no)) # filters phone number (used anonymous function x)
+        result = self._request(phone_no) # call request function and store the result in result as json pay load
         try:
             #print phone_no
-            return result['national_format'].count(' ') == 1, result['phone_number']
+            return result['national_format'].count(' ') == 1, result['phone_number'] # return national-format and phone number
         except:
             print "Bad Phone number "+str(phone_no)
             return False, phone_no
 
-    def _addCountryCode(self,phone_no):
-        phone_no=phone_no.replace(' ','').replace('(','').replace(')','')
-        if '0091' in phone_no[0:len(phone_no)/2+1]:
+    def _addCountryCode(self,phone_no):     # adds or correcting country code like +91        
+        phone_no=phone_no.replace(' ','').replace('(','').replace(')','') # removes all white space,(,) from phone number
+        if '0091' in phone_no[0:len(phone_no)/2+1]: # remove 0091 is given as country code 
             phone_no=phone_no.replace('0091','')
-        elif '091' in phone_no[0:len(phone_no)/2+1]:
+        elif '091' in phone_no[0:len(phone_no)/2+1]: # remove 091 is given as country code
             phone_no=phone_no.replace('091','')
         if phone_no.startswith('0'):
             phone_no = phone_no[1:]
-        if not phone_no.startswith('91') and not phone_no.startswith('+91'):
+        if not phone_no.startswith('91') and not phone_no.startswith('+91'): # prepends + to 91
             return '+91-'+phone_no
         return phone_no
 
@@ -104,22 +106,32 @@ class processMobile:
             phn_no_ls=''
             for i in range(1,6):
                 nphn=''
-                if row['Phone'+str(i)] is not None:
+                if row['Phone'+str(i)] is not None: # getting Phone1 column data from the row
+                    
+                    # Adding STD code to telephone no.
+                    
+                    if len(row['Phone'+str(i)])<9:
+                        city = row["City"]
+                        row['Phone'+str(i)] = processMobileSTDcode.processSTD(city,row['Phone'+str(i)])
+                    
                     y=(filter(lambda x: not x.isalpha(),safe_dec_enc(row['Phone'+str(i)])).strip().replace(' ',''))
-                    noslist1.append(y)
-                    row['Phone'+str(i)]=None
+                    noslist1.append(y)          # inserting that no. into noslist1
+                    row['Phone'+str(i)]=None    # removed the Phone number from the column
+                    
                     # nphn=row['Phone'+str(i)].replace(' ','').replace('(','').replace(')','').replace('-','')
                     # if len(nphn)>=20:
                     #     phn_no_ls+=
                     # phn_no_ls+=row['Phone'+str(i)]+','
 
             noslist1=filter(None,noslist1);
-            noslist1=list(set(noslist1))
+            noslist1=list(set(noslist1)) # creating a list out of noslist1
             #print 'noslist1',noslist1
+            
             for i in range(len(noslist1)):
                 # row['Phone'+str((i+1))]=noslist1[i]
-                phn_no_ls+=noslist1[i]+','
-            phn_no_ls=phn_no_ls.split(',')
+                phn_no_ls+=noslist1[i]+','  # converting the list into string separated by comma
+            
+            phn_no_ls=phn_no_ls.split(',')  # converting back to list
             phn_no_ls=filter(None,phn_no_ls)
 
             #phn_no_ls=map(str,phn_no_ls)
@@ -137,7 +149,7 @@ class processMobile:
                 if '/' not in no:
                     noslist.append(no)
                 elif '/' in no:
-                    splt_list=no.split('/')
+                    splt_list=no.split('/') # e.g. '70324xxxxx/45695xxxxx' to ['70324xxxxx','45695xxxxx']
                     for i in range(1,len(splt_list)):
                         if len(splt_list[i])>=10:
                             continue
@@ -179,14 +191,11 @@ class processMobile:
                     n = safe_dec_enc(row["Phone"+str(i)])
                     if not n:
                         continue
-
-                    if len(n)<9:
-                        city = safe_dec_enc(row["City"])
-                        n = processMobileSTDcode.processSTD(city,n)
-
+                                        
                     isMob,cleaned_no = self._isMobile(self._addCountryCode(n))
                     if isMob:
-                        cleaned_no= cleaned_no.replace('+91','')
+                        cleaned_no= cleaned_no.replace('+91','') # removing the country code to get the actual phone number 
+
                         if cleaned_no[0:2]=='91':
                             cleaned_no=cleaned_no[2:]
                         
